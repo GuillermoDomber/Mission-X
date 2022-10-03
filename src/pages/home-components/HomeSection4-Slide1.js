dc6f93e4f2eb3f72dbb060cdee468272d4e186b5
Guillermo Domber <guillermo.domber@gmail.com>
2022-10-04 08:18:08 +1300

    adding home page slides

---
+++ src/pages/home-components/HomeSection4-Slide1.js
@@ -0,0 +1,38 @@
+export default function Slide1() {
+  return (
+    <>
+      <div className="slides">
+        <div>Interlinking Pathways </div>
+        <div>
+          This programme gives us 5 important interlinking Learning Pathways.
+        </div>
+        <div>COMPUTATIONAL THINKING </div>
+        <div>
+          Within the programme the students are enabled to express problems and
+          form solutions that will be designed so a computer can be used to
+          create diverse and encapsulating applications.
+        </div>
+        <div>DEVELOPING DIGITAL OUTCOMES</div>
+        <div>
+          This programme is designed to strengthen the outcomes of each students
+          personally to form strong applications.
+        </div>
+        <div>DESIGNING PROCESSED OUTCOMES </div>
+        <div>
+          Students will be taught the ways of how outcomes are processed,
+          thought about, and produced.
+        </div>
+        <div>DEVELOP VISUAL AND SOCIAL COMMUNICATIONS </div>
+        <div>
+          Students will learn to design visually pleasing applications used to
+          both keep the user aware of what is happening on the screen.
+        </div>
+        <div>STRONG TECHNOLOGICAL PRACTICES </div>
+        <div>
+          The programme will show students the best practices to think and solve
+          the problems brought on by using technology.
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/home-components/HomeSection4-Slide3.js
@@ -0,0 +1,22 @@
+
+
+export default function Slide3() {
+    return (
+        <>
+        <div className="slides">
+        <div>Enhance key competencies</div>
+        <div>The programme enhances capabilities of students in the 5 Key Competencies identified in the New Zealand Curriculum:</div>
+        <div>THINKING</div>
+        <div>In particular the programme focused on problem solving, design thinking and computational thinking.</div>
+        <div>DISCERNING CODES</div>
+<div>Analysing language, symbols, and texts in order to understand and make sense of the codes in which knowledge is expressed.</div>
+<div>SELF-MANAGEMENT</div>
+<div>Projects and challenges are designed to motivate students to explore and experiment with self-motivation.</div>
+<div>RELATIONSHIPS WITH PEERS</div>
+<div>The programme is designed with unplugged sessions where students interact in a range of different situations, including things like being able to listen well, recognise different points of view, and share ideas.</div>
+<div>PARTICIPATION AND COLLABORATION</div>
+<div>The programme encourages students to be involved in communities, such as family, whanau, school, and contribute and make connections with other people.</div>
+        </div>
+        </>
+    )
+}
