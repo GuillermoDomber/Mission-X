97d27c880a714057b281975485d87b1284441285
jervinguinto <jervin.guinto052894@gmail.com>
2022-10-13 22:57:51 +1300

    updated Jervin branch

---
+++ src/pages/HeaderJg/HeaderJg.js
@@ -0,0 +1,87 @@
+import styles from "./HeaderJg.module.css";
+import logo from "../libPhoto/logo.png";
+import Mflag from "../libPhoto/Mflag.jpg";
+import NzFlag from "../libPhoto/NZFlag.jpg";
+import kidImg from "../libPhoto/RawiriFletcher.png";
+import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+export default function HeaderJG() {
+  const [show, setshow] = useState(false);
+
+  const showUL = function () {
+    setshow(!show);
+  };
+
+  window.onclick = function (e) {
+    if (!e.target.matches(".dropDown")) {
+    }
+    console.log(e)
+  };
+
+  let menuClose = useRef();
+  useEffect(() => {
+    let closeDropdown = (e) => {
+      if (!menuClose.current.contains(e.target)) {
+        setshow(false);
+      }
+    };
+    document.body.addEventListener("click", closeDropdown);
+
+    return()=>{
+      document.body.removeEventListener("click",closeDropdown) // go back to this code
+    }
+  });
+
+  return (
+    <div>
+      <nav className={styles.navBarHeader}>
+        <div className={styles.leftNav}>
+          <img src={logo} alt='logo'></img>
+        </div>
+        <div className={styles.centerNav}>
+          <p>HOME</p>
+          <p>PROJECTS</p>
+          <p>TEACHERS</p>
+        </div>
+        <div className={styles.rightNav}>
+          <div className={styles.topRight}>
+            <p className={styles.langText}>LANG</p>
+
+            <img src={NzFlag} alt='nzflag' ></img>
+            <img src={Mflag} alt='mflag'></img>
+          </div>
+
+          <div onClick={showUL} className={styles.downRight} ref={menuClose}>
+            {/* <div
+              onClick={showUL}
+              className={`styles.dropDown ${show ? "active" : "inactive"}`}
+            >
+               */}
+              <img className={styles.kidPhoto} src={kidImg} alt='kid' ></img>
+           
+              <p className={styles.Rflet}>RAWIRI FLETCHER</p>
+              {show === true ? (
+              <ul className={styles.dropDown}>
+                <li><Link className={styles.link} to='/StudentProfileViewer'>My Profile</Link></li>
+                <li>Settings</li>
+                <li>Log out</li>
+              </ul>
+            ) : (
+              ""
+            )}
+            {/* </div> */}
+            {/* <div className={styles.content}>
+              <ul>
+                <li>
+                  <Link to="./StudentProfileViewer">My Profile</Link>{" "}
+                </li>
+                <li>Settings</li>
+                <li>Log out</li>
+              </ul>
+            </div> */}
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
---
+++ src/pages/StudentProjectLibrary/ProjectsLibraryJG.js
@@ -0,0 +1,39 @@
+import styles from "./ProjectsLibraryJG.module.css";
+
+export default function ProjectsLibraryJG() {
+let projects =[
+  {
+image:"project1.png",
+name:"Introduction",
+difficulty:"BEGINNER | Animation",
+  }
+
+
+]
+
+
+
+  return (
+    <div className={styles.rightSide}>
+      <div className={styles.topButtons}>
+        <div className={styles.leftButtons}>
+          <button className={styles.begBtn}>BEGINNER</button>
+          <button className={styles.intBtn}>INTERMEDIATE</button>
+          <button className={styles.advBtn}>ADVANCED</button>
+        </div>
+        <div className={styles.rightButton}>
+          <span className={styles.show}>SHOW</span>
+          <button className={styles.number25}>25</button>
+          <button className={styles.number50}>50</button>
+          <button className={styles.number100}>100</button>
+        </div>
+      </div>
+
+      <div className={styles.bottomProjects}>
+
+
+
+      </div>
+      </div>
+  );
+}
