e40430253e746c46854d91a8342289ec36181d3d
jervinguinto <jervin.guinto052894@gmail.com>
2022-10-13 22:52:04 +1300

    updated Jervin branch

---
+++ src/pages/home-components/HomeSection4-Slide4.js
@@ -0,0 +1,22 @@
+
+
+export default function Slide4() {
+    return (
+        <>
+        <div className="homeSlides">
+        <div>IR4.0</div>
+        <div>Designed with IT industry experts, the programme develops the students to find applicable jobs and careers in the Fourth Industrial Revolution. (IR4.0)</div>
+        <div>LEARNING TO LEARN</div>
+        <div>The programme will set challenges at the end of every project to encourage students to explore and learn how to learn.</div>
+        <div>COMMUNITY ENGAGEMENT</div>
+        <div>The programme encourages students to be involved in the communities, such as family, friends, and in school, to contribute and make connections with other people.</div>
+        <div>CULTURAL DIVERSITY</div>
+        <div>This programme is designed in New Zealand and reflects NZ's cultural diversity.</div>
+        <div>INCLUSION</div>
+        <div>In particular the programme is designed with acknowledgement to the student's identities and talents, allowing them to be creative to their personal ability.</div>
+        <div>FUTURE FOCUS</div>
+        <div>The programme leads students to explore future themes such as artificial intelligence and augmented reality.</div>
+        </div>
+        </>
+    )
+}
