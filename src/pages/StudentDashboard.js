2f6efb8ea061c8429f44bd28a2be9e2ec81e62e9
Bonnie Wall <bm-wall@outlook.com>
2022-10-15 14:07:59 +1300

    Created StudentDashboard and shared component files and linked them to StudentDashboard

--- src/pages/StudentDashboard.js
+++ src/pages/StudentDashboard.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import styles from "././studentDashboard-components/StudentDashboard.module.css";
+import DashboardNavBar from "../components/DashboardNavBar";
+import DashboardSidebar from "../components/DashboardSidebar";
+import StudentDashboardHero from "./studentDashboard-components/StudentDashboard-hero";
+import DashboardFooter from "../components/DashboardFooter";
 
 export default function StudentDashboard() {
   return (
-    <div>StudentDashboard</div>
-  )
+    <div className={styles.pageContainer}>
+      <h1>StudentDashboard</h1>
+      <DashboardNavBar />
+
+      <main className={styles.mainHeroSection}>
+        <DashboardSidebar />
+        <StudentDashboardHero />
+      </main>
+
+      <DashboardFooter />
+    </div>
+  );
 }
---
+++ src/pages/studentDashboard-components/StudentDashboard-hero.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StudentDashboardHero() {
+  return (
+    <div>StudentDashboard-hero</div>
+  )
+}
